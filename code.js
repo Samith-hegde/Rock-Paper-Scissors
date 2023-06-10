4307837e899cd2945b217154a02fa669dd1df075
Samith Hegde <hegdesamith02@gmail.com>
2023-06-10 23:57:15 +0530

    Updated from prompt to button, alert to display

--- code.js
+++ code.js
@@ -1,65 +1,116 @@
 function getComputerChoice() {
-  let choices = ["ROCK", "PAPER", "SCISSORS"];
+  let choices = ["ASSASSIN", "WARRIOR", "MAGE"];
   
   let choice = choices[Math.floor(Math.random()*choices.length)];
   return choice;
 }
 
-function one_round(playerSelection, computerSelection) {
+function one_round(playerSelection) {
+    let computerSelection = getComputerChoice();
 
     if (playerSelection === computerSelection) {
-        return ["This round is a draw. Play again", 'draw'];
+        updateMessage('This round is a draw. Push button above to continue.');
+        endRound();
+        return;
     }
 
     let a = 0
 
-    if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+    if (playerSelection === "ASSASSIN" && computerSelection === "MAGE") {
         a++;
     }
 
-    if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    if (playerSelection === "WARRIOR" && computerSelection === "ASSASSIN") {
         a++;
     }
 
-    if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    if (playerSelection === "MAGE" && computerSelection === "WARRIOR") {
         a++;
     }
 
     if (a === 1) {
-        return [`You win. ${playerSelection} beats ${computerSelection}`, 'win']
+        updateMessage(`You win. ${playerSelection} beats ${computerSelection}`);
+        player++;
+        round++;
+        endRound();
+        return;
     }
     else if (a === 0) {
-        return [`You lose. ${computerSelection} beats ${playerSelection}`, 'lose']
+        updateMessage(`You lose. ${computerSelection} beats ${playerSelection}`);
+        computer++;
+        round++;
+        endRound();
+        return;
     }
+}
+
+function updateMessage(message) {
+    let gameMessage = document.querySelector('#gameMessages');
+    gameMessage.textContent = message;
+}
+
+function endRound() {
+    updatePlayerScoreboard();
+    updateComputerScoreboard();
+    toggleHidden();
+}
+
+function updatePlayerScoreboard() {
+    let playerScoreboard = document.querySelector('#playerScoreboard');
+    playerScoreboard.textContent = 'Player: ' + player;
+}
+
+function updateComputerScoreboard() {
+    let computerScoreboard = document.querySelector('#computerScoreboard');
+    computerScoreboard.textContent = 'Computer: ' + computer;
+}
+
+function reset() {
+    round = 0;
+    player = 0;
+    computer = 0;
 }
 
 function game() {
-    let player = 0;
-    let computer = 0;
-
-    while (player<5 && computer<5) {
-        let playerSelection = prompt("Type your choice: Rock, Paper, or Scissors: ");
-        playerSelection = playerSelection.toUpperCase();
-        let computerSelection = getComputerChoice();
-
-        alert(`You chose ${playerSelection}. The computer chose ${computerSelection}`)
-        let [result_string, result_word] = one_round(playerSelection, computerSelection);
-        alert(result_string);
-        if (result_word==='win') {
-            player++;
-        }  
-        else if (result_word==='lose') {
-            computer++;
-        }
-        alert(`Score: Player = ${player}; Computer = ${computer}`)
-    }
+    updateMessage('Starting round' + (round+1) + '. Make your selection');
+    toggleHidden();
+    updatePlayerScoreboard('Player: ' + player);
+    updateComputerScoreboard('Computer: ' + computer);
 
     if (player===5) {
-        alert('You have won the game')
+        updateMessage('You have won the game');
+        reset();
     }
     else if(computer===5) {
-        alert('You have lost the game')
+        updateMessage('You have lost the game');         
+        reset();
     }
 }
 
-game();
+function toggleHidden() {
+    var selectionButtonContainer = document.querySelector('#selectionButtonContainer');
+    if (selectionButtonContainer.style.display === "none") {
+        selectionButtonContainer.style.display = "flex";
+    } else {
+        selectionButtonContainer.style.display = "none";
+    }
+}
+
+let player = 0;
+let computer = 0;
+let round = 0;
+toggleHidden();
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let startButton = document.querySelector('#startButton');
+    startButton.addEventListener('click', function () {game()});
+
+    let assassinButton = document.querySelector('#assassinButton');
+    assassinButton.addEventListener('click', function() {one_round('ASSASSIN')});
+
+    let mageButton = document.querySelector('#mageButton');
+    mageButton.addEventListener('click', function() {one_round('MAGE')});
+
+    let warriorButton = document.querySelector('#warriorButton');
+    warriorButton.addEventListener('click', function() {one_round('WARRIOR')});
+});
